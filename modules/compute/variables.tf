ac35abfcd66a57a63033cc6de4bacee5c489d300
Adetunji <adetunji.adeshina@gmail.com>
2022-12-03 14:36:42 -0800

    added ssh path to compute variable

--- modules/compute/variables.tf
+++ modules/compute/variables.tf
@@ -8,13 +8,13 @@ variable "region" {
 variable "ssh_key_public" {
   type    = string
   #Replace this with the location of you public key .pub
-  default = "C:\\Users\\wessa\\.ssh\\id_rsa.pub"
+  default = "/Users/adetunjiadeshina/.ssh/id_rsa.pub"
 }
 
 variable "ssh_key_private" {
   type    = string
   #Replace this with the location of you private key
-  default = "C:\\Users\\wessa\\.ssh\\id_rsa"
+  default = "/Users/adetunjiadeshina/.ssh/id_rsa"
 }
 
 variable "subnet_ips" {}
